24bec5b96f9d6be83338b0b810c3868512f9299b
re-pawan <re-pawan@indicchain.com>
2022-04-16 13:14:07 +0530

    sign up

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Login from './src/screens/Login';
 import Sign from '../Task/src/screens/Sign';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import forgot from './src/screens/forgot';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () =>{
     <Stack.Navigator screenOptions={{headerShown:false}}>
      <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Sign" component={Sign} />
+      {/* <Stack.Screen name="forgot" component={forgot}/> */}
     </Stack.Navigator>
   </NavigationContainer>
   );
